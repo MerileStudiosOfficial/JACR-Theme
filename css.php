90082707309ebbeeef100a78ea3d21820882a08a
MerileStudiosOfficial <sonichedgehog1@hotmail.com>
2017-01-08 17:16:54 -0600

    Create css.php

---
+++ css.php
@@ -0,0 +1,10 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+?>
+{
+    "css": "https://example.com/example.css",
+    "social": {
+        "icon": "https://example.com/example.png",
+        "link": "https://exmaple.com"
+    }
+}
