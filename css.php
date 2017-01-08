d716a85acd970bfc9a43fab91bb7b0eeb11e7cd9
MerileStudiosOfficial <sonichedgehog1@hotmail.com>
2017-01-08 17:17:29 -0600

    Update css.php

--- css.php
+++ css.php
@@ -2,7 +2,7 @@
     header("Access-Control-Allow-Origin: *");
 ?>
 {
-    "css": "https://example.com/example.css",
+    "css": "https://raw.githubusercontent.com/MerileStudiosOfficial/JACR-Theme/master/yahboi",
     "social": {
         "icon": "https://example.com/example.png",
         "link": "https://exmaple.com"
